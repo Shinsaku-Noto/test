a70eb13ecdc9195b1760f9c2cb8ce3f01800c51e
Shinsaku-Noto <shinsaku32823gmail.com>
2023-08-18 15:56:04 +0900

    ブログ投稿詳細の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,13 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post){
+    public function index(Post $post)
+    {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
     }
 }
