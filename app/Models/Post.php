b251c5f2c7106eede6dd5527e937480fb01751b8
Shinsaku-Noto <shinsaku32823gmail.com>
2023-08-23 11:07:19 +0900

    リレーション

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,11 +15,17 @@ class Post extends Model
     protected $fillable = [
         'title',
         'body',
+        'category_id',
         ];
     
     public function getPaginateByLimit(int $limit_count = 3)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
